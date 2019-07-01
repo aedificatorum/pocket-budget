79f0769690afd6bf8db9e6656ce02e24b619126d
celinechole <github@celinechole.com>
2019-07-01 13:05:26 -0400

    add routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { useState }  from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import tw from "tailwind.macro";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core"
 import ExportTable from "./Components/ExportTable"
 import AddBudgetItem from "./Components/AddBudgetItem";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   const [dataToExport, setDataToExport] = useState([]);
 
-  const addRowToExport = ({date, reportingdate, currency, location, category, subcategory, to, amount, details, project}) => {
+  const addRowToExport = ({ date, reportingdate, currency, location, category, subcategory, to, amount, details, project }) => {
     const newData = dataToExport.slice();
     newData.push({
       date, reportingdate, currency, location, category, subcategory, to, amount, details, project
@@ -23,17 +24,17 @@ function App() {
 
   return (
     <React.Fragment>
-      <h1>
+      <h1 css={tw`text-4xl p-6`}>
         Pocket-Budget
       </h1>
-      <div>
-        <AddBudgetItem addNewItem={addRowToExport}/>
-      </div>
-      <div>
-        <ExportTable
-          dataToExport={dataToExport}
-          markDataAsExported={markDataAsExported}/>
-      </div>
+      <main>
+        <Switch>
+          <Route exact path='/' component={() => <AddBudgetItem addNewItem={addRowToExport} />} />
+          <Route path='/data' component={() => <ExportTable
+            dataToExport={dataToExport}
+            markDataAsExported={markDataAsExported} />} />
+        </Switch>
+      </main>
       <footer>
         Built by <a href="https://github.com/aedificatorum">Aedificatorum</a>.
       </footer>
