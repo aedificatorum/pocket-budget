91b2cd4cb41ee74339cfc830ebbc5aa985ce84fe
Timothy Addison <taddison@users.noreply.github.com>
2019-07-06 22:46:40 -0400

    Export filtering (#5)
    * add export filtering
    
    * implement set all exported

--- src/Components/InMemoryStore/index.js
+++ src/Components/InMemoryStore/index.js
@@ -1,15 +1,14 @@
 let items = [];
 
 const getPendingItems = () => {
-  return items.slice();
+  return items.filter(i => !i.exported);
 }
 
 const addItem = ({date, reportingdate, currency, location, category, subcategory, to, amount, details, project}) => {
   const id = Math.random() * 500000;
   items.push({
-    id, date, reportingdate, currency, location, category, subcategory, to, amount, details, project
+    id, date, reportingdate, currency, location, category, subcategory, to, amount, details, project, exported: false
   });
-  console.log(items)
 };
 
 const removeItem = (id) => {
@@ -17,10 +16,14 @@ const removeItem = (id) => {
 };
 
 const setAllExported = () => {
-  items = [];
+  items.map((item, i) => {
+    if(!item.exported) {
+      items[i].exported = true;
+    }
+  });
 };
 
-// Seed the store with a fake item
+// Seed the store with a few fake items
 const todayAsDefault = new Date().toISOString().substr(0, 10);
 
 addItem({
@@ -35,5 +38,21 @@ addItem({
   details: "",
   project: ""
 });
+
+addItem({
+  date: todayAsDefault,
+  reportingdate: todayAsDefault,
+  currency: "USD",
+  location: "New York",
+  category: "Food",
+  subcategory: "Restaurant",
+  to: "Chiptole",
+  amount: 12.49,
+  details: "",
+  project: ""
+});
+
+items[1].exported = true;
+
 
 export { getPendingItems, addItem, removeItem, setAllExported };
