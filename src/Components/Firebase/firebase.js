0acf4779c7623e1dfde6adde367e37aed1e2c658
Tim Addison <git@tjaddison.com>
2019-07-07 17:10:38 -0400

    Add firebase to project. Add shell components for auth and store

---
+++ src/Components/Firebase/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyD9CBg8CS9XHEH5ipIJMOIIWL7wAHecctk",
+    authDomain: "pocket-budget-prod.firebaseapp.com",
+    databaseURL: "https://pocket-budget-prod.firebaseio.com",
+    projectId: "pocket-budget-prod",
+    storageBucket: "pocket-budget-prod.appspot.com",
+    messagingSenderId: "770970338707",
+    appId: "1:770970338707:web:ac5c0ac7b3753210"
+  });
+
+export default firebase;
---
+++ src/Components/Firebase/firebaseStore.js
@@ -0,0 +1,13 @@
+import firebase from "./firebase";
+
+const db = firebase.database();
+
+//TODO: All of this
+const getPendingItems = () => { console.log(db)};
+const getItem = () => {};
+const addItem = () => {};
+const removeItem = () => {};
+const updateItem = () => {};
+const setAllExported = () => {};
+
+export { getPendingItems, getItem, addItem, removeItem, updateItem, setAllExported };
