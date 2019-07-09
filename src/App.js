9071c2e8a9550e7aa5361a17315d8f8d4cb0ed0e
Tim Addison <git@tjaddison.com>
2019-07-09 18:19:13 -0400

    Move login to method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import tw from "tailwind.macro";
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
@@ -32,51 +32,56 @@ const App = () => {
     setDataToExport(getPendingItems());
   }, []);
 
+  const login = () => {
+    setAuthState({ ...authState, userId: 123 });
+  }
+
   return !authState.userId ? (
-    <button onClick={() => setAuthState({...authState, userId: 123 })}>Login</button>
+    <button onClick={login}>Login</button>
   ) : (
-    <div css={tw`min-h-screen flex flex-col font-sansmx-auto ml-12 m-0 p-6`}>
-      <header>
-        <h1 css={tw`text-4xl p-6`}>
-          <Link to="/">Pocket-Budget</Link>
-        </h1>
-        <nav css={tw`p-6`}>
-          <ul css={tw`flex`}>
-            <li css={tw`mr-6`}>
-              <Link to="/" css={tw`text-blue-500 hover:text-blue-800`}>Home</Link>
-            </li>
-            <li css={tw`mr-6`}>
-              <Link to="/data" css={tw`text-blue-500 hover:text-blue-800`}>Export Data ({dataToExport.length})</Link>
-            </li>
-            <li css={tw`mr-6`}>
-              <Link to="/summary" css={tw`text-blue-500 hover:text-blue-800`}>Summary</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main css={tw`flex-grow p-6`}>
-        <Switch>
-          <Route exact path="/" component={() => <AddBudgetItem addNewItem={addRowToExport} />} />
-          <Route path="/data" component={() => <ExportTable
-            dataToExport={dataToExport}
-            markDataAsExported={markDataAsExported} />} />
-          <Route path="/summary" component={() => <SummaryTable
-            dataToExport={dataToExport}
-            deleteItem={deleteItem} />} />
-          <Route path="/edit/:id" component={(routeProps) =>
-            <AddBudgetItem
-              getItem={getItem}
-              id={routeProps.match.params.id}
-              updateItem={updateItem}
-            />
-          } />
-        </Switch>
-      </main>
-      <footer css={tw`w-full text-center border-t border-grey p-4`}>
-        Built by <a href="https://github.com/aedificatorum">Aedificatorum</a>.
+      <div css={tw`min-h-screen flex flex-col font-sansmx-auto ml-12 m-0 p-6`}>
+        <header>
+          <h1 css={tw`text-4xl p-6`}>
+            <Link to="/">Pocket-Budget</Link>
+          </h1>
+          <div css={tw`p-6`}>Logged In As: {authState.userId}</div>
+          <nav css={tw`p-6`}>
+            <ul css={tw`flex`}>
+              <li css={tw`mr-6`}>
+                <Link to="/" css={tw`text-blue-500 hover:text-blue-800`}>Home</Link>
+              </li>
+              <li css={tw`mr-6`}>
+                <Link to="/data" css={tw`text-blue-500 hover:text-blue-800`}>Export Data ({dataToExport.length})</Link>
+              </li>
+              <li css={tw`mr-6`}>
+                <Link to="/summary" css={tw`text-blue-500 hover:text-blue-800`}>Summary</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main css={tw`flex-grow p-6`}>
+          <Switch>
+            <Route exact path="/" component={() => <AddBudgetItem addNewItem={addRowToExport} />} />
+            <Route path="/data" component={() => <ExportTable
+              dataToExport={dataToExport}
+              markDataAsExported={markDataAsExported} />} />
+            <Route path="/summary" component={() => <SummaryTable
+              dataToExport={dataToExport}
+              deleteItem={deleteItem} />} />
+            <Route path="/edit/:id" component={(routeProps) =>
+              <AddBudgetItem
+                getItem={getItem}
+                id={routeProps.match.params.id}
+                updateItem={updateItem}
+              />
+            } />
+          </Switch>
+        </main>
+        <footer css={tw`w-full text-center border-t border-grey p-4`}>
+          Built by <a href="https://github.com/aedificatorum">Aedificatorum</a>.
       </footer>
-    </div>
-  );
+      </div>
+    );
 }
 
 export default App;
