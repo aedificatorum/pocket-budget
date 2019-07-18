788856254327b23d277cd56540ccd2d0e3a20396
celinechole <github@celinechole.com>
2019-07-18 17:19:52 -0400

    add react responsive

--- src/Components/SummaryTable.js
+++ src/Components/SummaryTable.js
@@ -3,6 +3,8 @@ import tw from "tailwind.macro";
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
 import { Link } from "react-router-dom";
+import MediaQuery from 'react-responsive'
+import { readdirSync } from "fs";
 
 const SummaryTable = ({ dataToExport, deleteItem }) => {
 
@@ -15,20 +17,38 @@ const SummaryTable = ({ dataToExport, deleteItem }) => {
   const exportRows = dataToExport.length === 0 ? null : (
     dataToExport.map((d, i) => {
       return (
+
+
         <tr key={i}>
-          <TDRow>{dateToString(d.date)}</TDRow>
-          <TDRow>{dateToString(d.reportingDate)}</TDRow>
-          <TDRow>{d.currency}</TDRow>
-          <TDRow>{d.location}</TDRow>
-          <TDRow>{d.category}</TDRow>
-          <TDRow>{d.subcategory}</TDRow>
-          <TDRow>{d.to}</TDRow>
-          {/* Entries default to positive as cost - Excel uses negative as cost */}
-          <TDRow>{d.amount * -1}</TDRow>
-          <TDRow>{d.details}</TDRow>
-          <TDRow>{d.project}</TDRow>
-          <TDRow><Link to={`/edit/${d.id}`}>Edit</Link></TDRow>
-          <TDRow><button onClick={() => deleteItem(d.id)}>Delete</button></TDRow>
+          <MediaQuery minDeviceWidth={1224}>
+            <TDRow>{dateToString(d.date)}</TDRow>
+            <TDRow>{dateToString(d.reportingDate)}</TDRow>
+            <TDRow>{d.currency}</TDRow>
+            <TDRow>{d.location}</TDRow>
+            <TDRow>{d.category}</TDRow>
+            <TDRow>{d.subcategory}</TDRow>
+            <TDRow>{d.to}</TDRow>
+            {/* Entries default to positive as cost - Excel uses negative as cost */}
+            <TDRow>{d.amount * -1}</TDRow>
+            <TDRow>{d.details}</TDRow>
+            <TDRow>{d.project}</TDRow>
+            <TDRow><Link to={`/edit/${d.id}`}>Edit</Link></TDRow>
+            <TDRow><button onClick={() => deleteItem(d.id)}>Delete</button></TDRow>
+          </MediaQuery>
+          <MediaQuery maxDeviceWidth={640}>
+
+            <TDRow>{dateToString(d.date)}</TDRow>
+            <TDRow>{dateToString(d.reportingDate)}</TDRow>
+            <TDRow>{d.currency}</TDRow>
+            <TDRow>{d.location}</TDRow>
+            <TDRow>{d.category}</TDRow>
+            <TDRow>{d.subcategory}</TDRow>
+            <TDRow>{d.to}</TDRow>
+            {/* Entries default to positive as cost - Excel uses negative as cost */}
+            <TDRow>{d.amount * -1}</TDRow>
+            <TDRow><Link to={`/edit/${d.id}`}>Edit</Link></TDRow>
+            <TDRow><button onClick={() => deleteItem(d.id)}>Delete</button></TDRow>
+          </MediaQuery>
         </tr>
       );
     })
@@ -40,28 +60,50 @@ const SummaryTable = ({ dataToExport, deleteItem }) => {
 
   return (
     <div>
-      <table css={tw`table-auto w-full text-left`}>
-        <thead>
-          <tr>
-            <TDHeader>Date</TDHeader>
-            <TDHeader>Reporting Date</TDHeader>
-            <TDHeader>Currency</TDHeader>
-            <TDHeader>Location</TDHeader>
-            <TDHeader>Category</TDHeader>
-            <TDHeader>Subcategory</TDHeader>
-            <TDHeader>To</TDHeader>
-            <TDHeader>Amount</TDHeader>
-            <TDHeader>Details</TDHeader>
-            <TDHeader>Project</TDHeader>
-            <TDHeader></TDHeader>
-            <TDHeader></TDHeader>
-
-          </tr>
-        </thead>
-        <tbody css={tw`hover:bg-grey-lighter`}>
-          {exportRows}
-        </tbody>
-      </table>
+      <MediaQuery minDeviceWidth={1224} device={{ deviceWidth: 1600 }}>
+        <table css={tw`table-auto w-full text-left`}>
+          <thead>
+            <tr>
+              <TDHeader>Date</TDHeader>
+              <TDHeader>Reporting Date</TDHeader>
+              <TDHeader>Currency</TDHeader>
+              <TDHeader>Location</TDHeader>
+              <TDHeader>Category</TDHeader>
+              <TDHeader>Subcategory</TDHeader>
+              <TDHeader>To</TDHeader>
+              <TDHeader>Amount</TDHeader>
+              <TDHeader>Details</TDHeader>
+              <TDHeader>Project</TDHeader>
+              <TDHeader></TDHeader>
+              <TDHeader></TDHeader>
+            </tr>
+          </thead>
+          <tbody css={tw`hover:bg-grey-lighter`}>
+            {exportRows}
+          </tbody>
+        </table>
+      </MediaQuery>
+      <MediaQuery maxDeviceWidth={640}>
+        <table css={tw`table-auto w-full text-left`}>
+          <thead>
+            <tr>
+              <TDHeader>Date</TDHeader>
+              <TDHeader>Reporting Date</TDHeader>
+              <TDHeader>Currency</TDHeader>
+              <TDHeader>Location</TDHeader>
+              <TDHeader>Category</TDHeader>
+              <TDHeader>Subcategory</TDHeader>
+              <TDHeader>To</TDHeader>
+              <TDHeader>Amount</TDHeader>
+              <TDHeader></TDHeader>
+              <TDHeader></TDHeader>
+            </tr>
+          </thead>
+          <tbody css={tw`hover:bg-grey-lighter`}>
+            {exportRows}
+          </tbody>
+        </table>
+      </MediaQuery>
     </div>
   );
 };
