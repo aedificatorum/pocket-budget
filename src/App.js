51f3901a19d93578a97901a7c451bd4033d32332
celinechole <github@celinechole.com>
2019-07-19 11:48:41 -0400

    update default value on the form

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { jsx } from "@emotion/core";
 import ExportTable from "./Components/ExportTable";
 import SummaryTable from "./Components/SummaryTable";
 import AddBudgetItem from "./Components/AddBudgetItem";
-import { addItem, removeItem, setAllExported, getPendingItems, getItem, updateItem, getCategories } from "./Components/Firebase";
-import { setupAuth, signIn, signOut } from "./Components/Firebase"
+import { addItem, removeItem, setAllExported, getPendingItems, getItem, updateItem, getCategories } from "./Components/InMemory";
+import { setupAuth, signIn, signOut } from "./Components/InMemory"
 import { Switch, Route, Link } from "react-router-dom";
 import { AuthStateContext } from "./Components/AuthStateProvider";
 
