a81eed0cf690c64dbac4d21107a7ccb0ecfa3698
celinechole <github@celinechole.com>
2019-07-21 17:32:32 -0400

    update login layout

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import { jsx } from "@emotion/core";
 import ExportTable from "./Components/ExportTable";
 import SummaryTable from "./Components/SummaryTable";
 import AddBudgetItem from "./Components/AddBudgetItem";
-import QuickAdd from "./Components/AddBudgetItem/QuickAdd"
-import { addItem, removeItem, setAllExported, getPendingItems, getItem, updateItem, getCategories } from "./Components/Firebase";
-import { setupAuth, signIn, signOut } from "./Components/Firebase"
+import QuickAdd from "./Components/AddBudgetItem/QuickAdd";
+import {
+  addItem,
+  removeItem,
+  setAllExported,
+  getPendingItems,
+  getItem,
+  updateItem,
+  getCategories
+} from "./Components/Firebase";
+import { setupAuth, signIn, signOut } from "./Components/Firebase";
 import { Switch, Route, Link } from "react-router-dom";
 import { AuthStateContext } from "./Components/AuthStateProvider";
 
@@ -18,27 +26,27 @@ const App = () => {
 
   const updateState = async () => {
     setDataToExport(await getPendingItems());
-  }
+  };
 
   const addRowToExport = async (id, item) => {
     await addItem(item);
     updateState();
-  }
+  };
 
   const editItem = async (id, item) => {
     await updateItem(id, item);
     updateState();
-  }
+  };
 
-  const deleteItem = async (id) => {
+  const deleteItem = async id => {
     await removeItem(id);
     updateState();
-  }
+  };
 
   const markDataAsExported = async () => {
     await setAllExported();
     updateState();
-  }
+  };
 
   useEffect(() => {
     updateState();
@@ -48,12 +56,12 @@ const App = () => {
     const getCategoriesAsync = async () => {
       const cats = await getCategories();
       if (isSubscribed) {
-        setCategories(cats)
+        setCategories(cats);
       }
-    }
+    };
 
     getCategoriesAsync();
-    return () => isSubscribed = false;
+    return () => (isSubscribed = false);
   }, []);
 
   useEffect(() => {
@@ -63,70 +71,121 @@ const App = () => {
   }, [setAuthState]);
 
   return !authState.userId ? (
-    <div css={tw`justify-center bg-gray-500 min-h-screen`}>
-      <h1 css={tw`flex p-4 justify-center items-center text-3xl md:text-4xl`}>Pocket-Budget</h1>
-      <div css={tw`flex justify-center`}>
-        <img src="Building-budget.jpg" alt="Budget Login Logo" css={tw`rounded-full h-32 w-32 md:h-64 md:w-64`} />
-      </div>
-      <div css={tw`flex min-h-screen justify-center items-center align-bottom`}>
-        <button css={tw`font-medium text-2xl`}onClick={signIn}>Login</button>
+    <div css={tw`flex flex-col bg-gray-500 h-screen`}>
+        <h1 css={tw`flex p-6 justify-center text-4xl md:text-6xl`}>
+          Pocket-Budget
+        </h1>
+        <div css={tw`flex-1 mx-auto`}>
+          <img
+            src="Building-budget.jpg"
+            alt="Budget Login Logo"
+            css={tw`rounded-full h-32 w-32 md:h-64 md:w-64 self-center`}
+          />
+        </div>
+        <div css={tw`flex mx-auto p-12`}>
+          <button css={tw`font-medium text-2xl md:text-4xl`} onClick={signIn}>
+            Login
+          </button>
       </div>
     </div>
   ) : (
-      <div css={tw`min-h-screen flex flex-col font-sansmx-auto ml-12 m-0 p-6`}>
-        <header>
-          <img css={tw`flex rounded-full w-16 h-16`} src={authState.userPhoto.toString()} alt="User Avatar"/>
-          <div css={tw`p-6`}>Welcome {authState.userId}.
-            <button css={tw`p-6`} onClick={signOut}>Logout</button>
-            <button css={tw`p-6`} onClick={updateState}>Refresh</button>
-          </div>
-          <nav css={tw`p-6`}>
-            <ul css={tw`flex`}>
-              <li css={tw`mr-6`}>
-                <Link to="/" css={tw`text-blue-500 hover:text-blue-800`}>Home</Link>
-              </li>
-              <li css={tw`mr-6`}>
-                <Link to="/quickadd" css={tw`text-blue-500 hover:text-blue-800`}>Quick Add</Link>
-              </li>
-              <li css={tw`mr-6`}>
-                <Link to="/data" css={tw`text-blue-500 hover:text-blue-800`}>Export Data ({dataToExport.length})</Link>
-              </li>
-              <li css={tw`mr-6`}>
-                <Link to="/summary" css={tw`text-blue-500 hover:text-blue-800`}>Summary</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <main css={tw`flex-grow`}>
-          <Switch>
-            <Route exact path="/" component={() =>
+    <div css={tw`min-h-screen flex flex-col font-sansmx-auto ml-12 m-0 p-6`}>
+      <header>
+        <img
+          css={tw`flex rounded-full w-16 h-16`}
+          src={authState.userPhoto.toString()}
+          alt="User Avatar"
+        />
+        <div css={tw`p-6`}>
+          Welcome {authState.userId}.
+          <button css={tw`p-6`} onClick={signOut}>
+            Logout
+          </button>
+          <button css={tw`p-6`} onClick={updateState}>
+            Refresh
+          </button>
+        </div>
+        <nav css={tw`p-6`}>
+          <ul css={tw`flex`}>
+            <li css={tw`mr-6`}>
+              <Link to="/" css={tw`text-blue-500 hover:text-blue-800`}>
+                Home
+              </Link>
+            </li>
+            <li css={tw`mr-6`}>
+              <Link to="/quickadd" css={tw`text-blue-500 hover:text-blue-800`}>
+                Quick Add
+              </Link>
+            </li>
+            <li css={tw`mr-6`}>
+              <Link to="/data" css={tw`text-blue-500 hover:text-blue-800`}>
+                Export Data ({dataToExport.length})
+              </Link>
+            </li>
+            <li css={tw`mr-6`}>
+              <Link to="/summary" css={tw`text-blue-500 hover:text-blue-800`}>
+                Summary
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main css={tw`flex-grow`}>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={() => (
               <AddBudgetItem
                 saveItem={addRowToExport}
                 categories={categories}
-              />} />
-            <Route exact path="/quickadd" component={() => <QuickAdd saveItem={addRowToExport} categories={categories} />} />
-            <Route path="/data" component={() => <ExportTable
-              dataToExport={dataToExport}
-              markDataAsExported={markDataAsExported} />} />
-            <Route path="/summary" component={() => <SummaryTable
-              dataToExport={dataToExport}
-              deleteItem={deleteItem} />} />
-            <Route path="/edit/:id" component={(routeProps) =>
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/quickadd"
+            component={() => (
+              <QuickAdd saveItem={addRowToExport} categories={categories} />
+            )}
+          />
+          <Route
+            path="/data"
+            component={() => (
+              <ExportTable
+                dataToExport={dataToExport}
+                markDataAsExported={markDataAsExported}
+              />
+            )}
+          />
+          <Route
+            path="/summary"
+            component={() => (
+              <SummaryTable
+                dataToExport={dataToExport}
+                deleteItem={deleteItem}
+              />
+            )}
+          />
+          <Route
+            path="/edit/:id"
+            component={routeProps => (
               <AddBudgetItem
                 getItem={getItem}
                 id={routeProps.match.params.id}
                 saveItem={editItem}
                 categories={categories}
-                returnAction={() => routeProps.history.push('/summary')}
+                returnAction={() => routeProps.history.push("/summary")}
               />
-            } />
-          </Switch>
-        </main>
-        <footer css={tw`w-full text-center border-t border-grey p-4`}>
-          Built by <a href="https://github.com/aedificatorum">Aedificatorum</a>.
+            )}
+          />
+        </Switch>
+      </main>
+      <footer css={tw`w-full text-center border-t border-grey p-4`}>
+        Built by <a href="https://github.com/aedificatorum">Aedificatorum</a>.
       </footer>
-      </div>
-    );
-}
+    </div>
+  );
+};
 
 export default App;
