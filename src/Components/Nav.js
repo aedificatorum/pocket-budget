6d1289b67e22d3ff56194da1a646c0fd78ca742a
Tim Addison <git@tjaddison.com>
2019-12-08 18:42:21 -0500

    finish removing tailwind from Nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,37 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import tw from "tailwind.macro";
-/** @jsx jsx */
-import { jsx } from "@emotion/core";
 import styled from "styled-components";
 
+// TODO: Pull this from theme context
 const StyledNavLink = styled.li`
   color: #4a5568;
   margin-right: 1.5rem;
 `;
 
-const NavLink = ({to, label, hideOnMobile = false}) => {
-  const className = hideOnMobile ? "hide-on-mobile" : ""
-  
+// TODO: Get background from theme context
+const NavContainer = styled.nav`
+  padding: 1.5rem 1rem 1rem 1rem;
+  background-color: #feebc8;
+  position: sticky;
+  top: 0;
+
+  ul {
+    display: flex;
+  }
+`;
+
+const NavLink = ({ to, label, hideOnMobile = false }) => {
+  const className = hideOnMobile ? "hide-on-mobile" : "";
+
   return (
-  <StyledNavLink className={className}>
-    <Link to={to}>
-      {label}
-    </Link>
-  </StyledNavLink>
-)
-}
+    <StyledNavLink className={className}>
+      <Link to={to}>{label}</Link>
+    </StyledNavLink>
+  );
+};
 
 const Nav = ({ exportItemCount }) => {
   return (
-    <nav css={tw`p-4 pt-6 bg-orange-200 sticky top-0`}>
-      <ul css={tw`flex`}>
+    <NavContainer>
+      <ul>
         <NavLink to="/" label="1-Click" />
         <NavLink to="/fullform" label="Add" />
-        <NavLink to="/data" label={`Export Data (${exportItemCount})`} hideOnMobile />
+        <NavLink
+          to="/data"
+          label={`Export Data (${exportItemCount})`}
+          hideOnMobile
+        />
         <NavLink to="/summary" label="Summary" />
       </ul>
-    </nav>
+    </NavContainer>
   );
 };
 
