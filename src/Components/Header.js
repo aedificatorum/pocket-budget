011c84db7f99f23a6136040666844778e55bc00f
celinechole <github@celinechole.com>
2019-12-08 19:04:00 -0500

    create header container

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,29 +6,49 @@ import { Link } from "react-router-dom";
 import { AuthStateContext } from "./AuthStateProvider";
 import { signOut } from "./Auth";
 import Nav from "./Nav";
+import styled from 'styled-components'
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 0.5rem;
+  background-color: #f6ad55;
+  color: white;
+
+  div:first-child {
+    font-weight: 600;
+    flex-grow: 1;
+    font-size: 2rem;
+    margin-left: 1rem;
+  }
+
+  div:last-child {
+    font-size: 1.25rem;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+`;
 
 const Header = ({ dataToExport }) => {
   const [authState] = useContext(AuthStateContext);
 
   return (
     <header>
-      <div css={tw`flex flex-row object-center p-2 bg-orange-400`}>
-        <img
-          css={tw`flex rounded-full w-16 h-16 object-center shadow-md`}
-          src={authState.userPhoto}
-          alt="User Avatar"
-        />
-        <div css={tw`flex p-2 text-white text-lg w-4/6 items-center`}>
-          <span css={tw`hidden md:inline-block mr-1`}>Welcome </span>{" "}
+      <HeaderContainer>
+        <div>Pocket Budget</div>
+        <div>
           {authState.userName}
         </div>
-        <button css={tw`flex w-1/6`} onClick={signOut}>
+        {/* <button css={tw`flex w-1/6`} onClick={signOut}>
           <img
             src="https://img.icons8.com/ios-glyphs/30/000000/logout-rounded-down.png"
             alt="logout-icon"
           />
-        </button>
-      </div>
+        </button> */}
+      </HeaderContainer>
       <Nav exportItemCount={dataToExport.length} />
     </header>
   );
