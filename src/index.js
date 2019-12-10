5654fa5f2dfa5d269b381faaa7dc67eef12f14f0
celinechole <github@celinechole.com>
2019-12-10 09:51:48 -0500

    remove tailwind from app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/tailwind.out.css";
+import "./styles/style.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
