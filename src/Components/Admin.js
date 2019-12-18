044d80a804b3fbcb8dcdb44a7932c789650735ee
celinechole <github@celinechole.com>
2019-12-18 10:21:20 -0500

    add border

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -27,7 +27,7 @@ const admin = ({ categories }) => {
     @media (max-width: ${props => props.theme.breakpoint}) {
       margin: 1rem 2rem;
       font-size: 1rem;
-      height: 450px;
+      height: 500px;
       flex-direction: column;
       h2 {
         font-size: 1.25rem;
@@ -51,10 +51,17 @@ const admin = ({ categories }) => {
     }
   `;
 
+  const BorderStyle = styled.div`
+    border: .125rem solid ${props => props.theme.textNormal};
+    margin-top: .5rem;
+    margin-bottom: .5rem;
+  `
+
   return (
     <AdminContainer>
       <section>
         <h2>Categories</h2>
+        <BorderStyle></BorderStyle>
         {categories.map((category, i) => {
           return (
             <ul key={i}>
@@ -65,6 +72,7 @@ const admin = ({ categories }) => {
       </section>
       <section>
         <h2>Defaults (Local Storage)</h2>
+        <BorderStyle></BorderStyle>
         {localStorageKeys.map(k => {
           return (
             <div key={k}>
