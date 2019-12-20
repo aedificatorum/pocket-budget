aae0b8539fa7c04677c4d3d416a75ff0e19ec5eb
celinechole <github@celinechole.com>
2019-12-19 19:30:49 -0500

    add details per currency

--- src/Components/OverviewCard.js
+++ src/Components/OverviewCard.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const OverviewCardLayout = styled.div`
   display: flex;
@@ -8,23 +8,31 @@ const OverviewCardLayout = styled.div`
   padding: 1rem;
   box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
     0 4px 6px -2px rgba(0, 0, 0, 0.05);
-`
+    
+`;
 
-
-export const OverviewCard = ({items, currency}) => {
+export const OverviewCard = ({ items, currency }) => {
   const totalPerCategory = items.reduce((acc, items) => {
-    if(acc[items.category]) {
-      acc[items.category] += Math.round(items.amount)
+    if (acc[items.category]) {
+      acc[items.category] += Math.round(items.amount);
     } else {
-      acc[items.category] = Math.round(items.amount)
+      acc[items.category] = Math.round(items.amount);
     }
-    return acc
-  }, {})
+    return acc;
+  }, {});
 
   return (
     <OverviewCardLayout>
-      {`${items.length} transactions in ${currency}`}
-      {Object.values(totalPerCategory)}
+      <div style={{marginBottom:"1rem", fontWeight:"600", alignSelf:"center", fontSize:"1.25rem"}}>
+        {`${items.length} transactions in ${currency}`}
+      </div>
+      {Object.keys(totalPerCategory).map(key => {
+        return (
+          <div key={key}>
+            {key}: <div style={{display:"inline"}}>{totalPerCategory[key]}</div>
+          </div>
+        );
+      })}
     </OverviewCardLayout>
-  )
-}
+  );
+};
