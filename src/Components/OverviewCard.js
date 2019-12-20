c253347f433eaece049789800071c31e4745c8d4
celinechole <github@celinechole.com>
2019-12-19 19:36:44 -0500

    sort category

--- src/Components/OverviewCard.js
+++ src/Components/OverviewCard.js
@@ -4,11 +4,10 @@ import styled from "styled-components";
 const OverviewCardLayout = styled.div`
   display: flex;
   flex-direction: column;
-  margin: .5rem;
+  margin: 0.5rem;
   padding: 1rem;
   box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
     0 4px 6px -2px rgba(0, 0, 0, 0.05);
-    
 `;
 
 export const OverviewCard = ({ items, currency }) => {
@@ -21,15 +20,33 @@ export const OverviewCard = ({ items, currency }) => {
     return acc;
   }, {});
 
+  const sortedTotal = [];
+
+  for (let category in totalPerCategory) {
+    sortedTotal.push([category, totalPerCategory[category]]);
+  }
+
+  sortedTotal.sort((a, b) => {
+    return b[1] - a[1];
+  });
+
   return (
     <OverviewCardLayout>
-      <div style={{marginBottom:"1rem", fontWeight:"600", alignSelf:"center", fontSize:"1.25rem"}}>
+      <div
+        style={{
+          marginBottom: "1rem",
+          fontWeight: "600",
+          alignSelf: "center",
+          fontSize: "1.25rem"
+        }}
+      >
         {`${items.length} transactions in ${currency}`}
       </div>
-      {Object.keys(totalPerCategory).map(key => {
+      {sortedTotal.map(category => {
         return (
-          <div key={key}>
-            {key}: <div style={{display:"inline"}}>{totalPerCategory[key]}</div>
+          <div key={category[0]}>
+            {category[0]}:{" "}
+            <div style={{ display: "inline" }}>{category[1]}</div>
           </div>
         );
       })}
