336a7215f6aeff775eff90e3d5b3d188a75465fe
celinechole <github@celinechole.com>
2019-12-21 15:53:08 -0500

    fix subcategory style

--- src/Components/Overview/OverviewCard.js
+++ src/Components/Overview/OverviewCard.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { FormattedNumber } from "react-intl";
 
 const categoryEmoji = {
-  'Food': '🍲',
-  'Health': '⚕️',
-  'House': '🏠',
-  'Income': '💵',
-  'Miscellaneous': '🧐',
-  'Entertainment': '😊',
-  'Personal': '👫',
-  'Travel': '🧳',
-}
+  Food: "🍲",
+  Health: "⚕️",
+  House: "🏠",
+  Income: "💵",
+  Miscellaneous: "🧐",
+  Entertainment: "😊",
+  Personal: "👫",
+  Travel: "🧳"
+};
 
 const OverviewCardLayout = styled.div`
   display: flex;
@@ -26,11 +26,77 @@ const TableRowStyle = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding: .5rem;
-  border-bottom: .0625rem solid lightgrey;
+  padding: 0.5rem;
+  border-bottom: 0.0625rem solid lightgrey;
   font-size: 1.125rem;
   color: ${props => props.theme.textNormal};
 `;
+
+const SubCategoryStyle = styled.div`
+  margin: 0.5rem .5rem 0rem .5rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const SubCategoryList = ({ items, currency }) => {
+  const subCatTotal = items.reduce((acc, item) => {
+    if (acc[item.subcategory]) {
+      acc[item.subcategory] += item.amount;
+    } else {
+      acc[item.subcategory] = item.amount;
+    }
+    return acc;
+  }, {});
+  return (
+    <>
+      {Object.keys(subCatTotal).map(subCat => {
+        return (
+          <SubCategoryStyle key={subCat}>
+            <div>{subCat}</div>
+            <div>
+              <FormattedNumber
+                style="currency"
+                currency={currency}
+                value={subCatTotal[subCat]}
+              />
+            </div>
+          </SubCategoryStyle>
+        );
+      })}
+      {/* {items.map(item => {
+        return <div key={item.id}>{item.subcategory}</div>;
+      })} */}
+    </>
+  );
+};
+
+const CategoryCard = ({ items, category, currency }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const categorySum = items.reduce((acc, item) => {
+    return acc + item.amount;
+  }, 0);
+  return (
+    <>
+      <TableRowStyle>
+        <button onClick={() => setIsExpanded(e => !e)}>
+          {categoryEmoji[category]} {category}
+        </button>
+        <div style={{fontWeight: isExpanded ? "600" : "normal"}}>
+          <FormattedNumber
+            style="currency"
+            currency={currency}
+            value={categorySum}
+          />
+        </div>
+      </TableRowStyle>
+      <div style={{ display: isExpanded ? "block" : "none", marginBottom: "1.5rem" }}>
+        <SubCategoryList items={items} currency={currency} />
+      </div>
+    </>
+  );
+};
 
 export const OverviewCard = ({ items, currency }) => {
   const totalPerCategory = items.reduce((acc, items) => {
@@ -45,11 +111,11 @@ export const OverviewCard = ({ items, currency }) => {
   const sortedTotal = [];
 
   for (let category in totalPerCategory) {
-    sortedTotal.push([category, totalPerCategory[category]]);
+    sortedTotal.push({ category, total: totalPerCategory[category] });
   }
 
   sortedTotal.sort((a, b) => {
-    return b[1] - a[1];
+    return b.total - a.total;
   });
 
   return (
@@ -66,16 +132,12 @@ export const OverviewCard = ({ items, currency }) => {
       </div>
       {sortedTotal.map(category => {
         return (
-          <TableRowStyle key={category[0]}>
-            <div>{categoryEmoji[category[0]]} {category[0]}</div>
-            <div>
-              <FormattedNumber
-                style="currency"
-                currency={currency}
-                value={category[1]}
-              />
-            </div>
-          </TableRowStyle>
+          <CategoryCard
+            key={category.category}
+            items={items.filter(item => item.category === category.category)}
+            category={category.category}
+            currency={currency}
+          />
         );
       })}
     </OverviewCardLayout>
