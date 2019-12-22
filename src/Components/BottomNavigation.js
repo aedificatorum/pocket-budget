c0851704b8b928593235389378b67b3ec4f18a0f
celinechole <github@celinechole.com>
2019-12-22 15:28:02 -0500

    implement bottom nav on mobile

---
+++ src/Components/BottomNavigation.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from "styled-components";
+
+const BottomNavigationContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  padding: .75rem;
+  position: fixed;
+  bottom: 0;
+  background-color: white;
+  border-top: 0.0625rem solid lightgrey;
+  box-shadow: 0px 0 5px rgba(0, 0, 0, 0.2);
+
+  @media (min-width: ${props => props.theme.breakpoint}) {
+    display: none;
+  }
+`
+
+const StyledLink = styled(Link)`
+  display: flex;
+  margin: auto;
+`;
+
+export const BottomNavigation = () => {
+  return (
+    <BottomNavigationContainer>
+      <StyledLink to="/">Home</StyledLink>
+      <StyledLink to='/overview'>Overview</StyledLink>
+      <StyledLink to='/'>?</StyledLink>
+    </BottomNavigationContainer>
+  )
+}
