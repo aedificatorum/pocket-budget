43164f6c16ef813c04b5d2159114d6412814dd61
Tim Addison <git@tjaddison.com>
2019-12-31 17:55:09 -0500

    move uuid function

--- src/Components/Store/inMemoryStore.js
+++ src/Components/Store/inMemoryStore.js
@@ -1,4 +1,5 @@
 import _ from "lodash";
+import { newId } from "../../Utils/idUtils";
 import moment from "moment";
 
 let items = [];
@@ -192,19 +193,9 @@ const subcategories = [
   },
 ];
 
-// https://stackoverflow.com/a/2117523
-function uuidv4() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-    (
-      c ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-    ).toString(16)
-  );
-}
-
 const accounts = subcategories.map(subcat => {
   return {
-    id: uuidv4(),
+    id: newId(),
     name: subcat.subcategory,
     isIncome: subcat.isIncome ? true : false,
     category: subcat.category
