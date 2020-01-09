f39d786a39d20ab570e3cc9a4c59510e3788d8b7
celinechole <github@celinechole.com>
2020-01-09 14:49:00 -0500

    more

--- src/Components/DatePickers/PeriodPicker.js
+++ src/Components/DatePickers/PeriodPicker.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
+import styled from "styled-components";
 import MonthPicker from "./MonthPicker";
-import {
-  ticksToShortDateWithYear,
-  getToday
-} from "../../Utils/dateUtils";
+import { ticksToShortDateWithYear, getToday } from "../../Utils/dateUtils";
+
+const SelectYourViewStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: .5rem 1rem .5rem 1rem;
+  font: 1.25rem;
+  margin-bottom: 1.25rem;
+  /* border: black solid 0.625rem; */
+  /* box-shadow: 0 10px 15px -3px rgba(0,0,0,0.1), 0 4px 6px -2px rgba(0,0,0,0.05); */
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+`;
 
 const DateRanges = {
   CalendarMonth: "calendarmonth",
@@ -107,7 +116,8 @@ const PeriodPicker = ({ ticks, setTicks }) => {
   useEffect(() => {
     if (ticks.fromTicks === null || ticks.toTicks === null) {
       setTicks({
-        fromTicks: moment.utc([yearMonth.year, yearMonth.month, 1]).unix() * 1000,
+        fromTicks:
+          moment.utc([yearMonth.year, yearMonth.month, 1]).unix() * 1000,
         toTicks:
           moment
             .utc([yearMonth.year, yearMonth.month, 1])
@@ -115,26 +125,44 @@ const PeriodPicker = ({ ticks, setTicks }) => {
             .unix() * 1000
       });
     }
-  }, [ticks.fromTicks, ticks.toTicks, yearMonth.year, yearMonth.month, setTicks]);
+  }, [
+    ticks.fromTicks,
+    ticks.toTicks,
+    yearMonth.year,
+    yearMonth.month,
+    setTicks
+  ]);
+
+  const [isExpanded, setIsExpanded] = useState(false)
 
   return (
     <div>
-      <select onChange={handleRangeChange}>
-        <option value={DateRanges.CalendarMonth}>Month</option>
-        <option value={DateRanges.YearToDate}>Year to Date</option>
-        <option value={DateRanges.LastCalendarMonth}>Last Month</option>
-        <option value={DateRanges.LastThreeCalendarMonths}>
-          Last 3 Months
-        </option>
-        <option value={DateRanges.LastYear}>Last Year</option>
-      </select>
-      {rangeType === DateRanges.CalendarMonth && (
-        <MonthPicker yearMonth={yearMonth} updateMonth={updateMonth} />
-      )}
-      <div>
-        {ticksToShortDateWithYear(ticks.fromTicks)} -{" "}
-        {ticksToShortDateWithYear(ticks.toTicks)}
+      <div style={{display: 'flex', justifyContent: "space-around", marginBottom:"1rem"}}>
+    <MonthPicker yearMonth={yearMonth} updateMonth={updateMonth} />
+    <button onClick={() => setIsExpanded(e => !e)} style={{display:"block", fontSize: "1.5rem"}}>➕</button>
       </div>
+    <div style={{ display: isExpanded? "block": "none"}}>
+      <SelectYourViewStyle>
+        <div style={{alignSelf: "center"}}>Select your view</div>
+        <select onChange={handleRangeChange} style={{textAlign: "right", padding: ".75rem", backgroundColor: "white"}}>
+          <option value={DateRanges.CalendarMonth}>Month</option>
+          <option value={DateRanges.YearToDate}>Year to Date</option>
+          <option value={DateRanges.LastThreeCalendarMonths}>
+            Last 3 Months
+          </option>
+          <option value={DateRanges.LastYear}>Last Year</option>
+        </select>
+      </SelectYourViewStyle>
+    </div>
+      {/* {rangeType === DateRanges.CalendarMonth && (
+        <MonthPicker yearMonth={yearMonth} updateMonth={updateMonth} />
+      )} */}
+      {rangeType !== DateRanges.CalendarMonth && (
+        <div style={{ textAlign: "center", font:"1.25rem", fontWeight:"600" }}>
+          {ticksToShortDateWithYear(ticks.fromTicks)} -{" "}
+          {ticksToShortDateWithYear(ticks.toTicks)}
+        </div>
+      )}
     </div>
   );
 };
