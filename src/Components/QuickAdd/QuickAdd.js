56a213dc49f3304d2989394fad57f515ef9c5804
Tim Addison <git@tjaddison.com>
2020-01-13 18:31:51 -0500

    expand to To

--- src/Components/QuickAdd/QuickAdd.js
+++ src/Components/QuickAdd/QuickAdd.js
@@ -13,41 +13,66 @@ const CategoryGroup = styled.li`
   margin-bottom: 1rem;
 `;
 
+const AccountList = styled.ul`
+  margin-left: 1rem;
+`;
+
+const ToList = styled.ul`
+  margin-left: 1rem;
+`;
+
 const QuickAdd = ({ accounts }) => {
   const [location, setLocation] = useState("");
 
   const categories = _.groupBy(accounts, "category");
-  
-  const updateLocation = (clickedLocation) => {
-    if(clickedLocation === location) {
+
+  const updateLocation = clickedLocation => {
+    // If we click the same location collapse instead
+    if (clickedLocation === location) {
       setLocation("");
     } else {
       setLocation(clickedLocation);
     }
-  }
+  };
 
   return (
     <Container>
       <ul>
         {Object.keys(categories).map(category => {
           return (
-            <CategoryGroup>
-              <button onClick={() => updateLocation(category)}>{category}</button>
-              {location.startsWith(`${category}`) && categories[category].map(account => {
-                return (
-                  <Link
-                    key={account.accountId}
-                    to={{
-                      pathname: "/fullform",
-                      initialAccountId: account.accountId
-                    }}
-                  >
-                    <li>
-                      {account.name} {account.isIncome ? "(Income)" : null}
-                    </li>
-                  </Link>
-                );
-              })}
+            <CategoryGroup key={category}>
+              <button onClick={() => updateLocation(category)}>
+                {category}
+              </button>
+              {location.startsWith(`${category}`) &&
+                categories[category].map(account => {
+                  return (
+                    <AccountList key={account.accountId}>
+                      <li>
+                        <button onClick={() => updateLocation(`${category}.${account.name}`)}>
+                          {account.name} {account.isIncome ? "(Income)" : null}
+                        </button>
+                        {" "}
+                        <Link
+                          key={account.accountId}
+                          to={{
+                            pathname: "/fullform",
+                            initialAccountId: account.accountId
+                          }}
+                        >
+                          <span role="img" aria-label="Add new item">
+                            ➕
+                          </span>
+                        </Link>
+                      </li>
+                      {location.startsWith(`${category}.${account.name}`) && (
+                        <ToList>
+                          <li>Hey you clicked on the account!</li>
+                        </ToList>
+                      )}
+                    </AccountList>
+                  );
+                })}
             </CategoryGroup>
           );
         })}
