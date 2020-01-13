61e65ae2f3f4985f797a1cf7b2b2f76848a6ab90
Tim Addison <git@tjaddison.com>
2020-01-12 20:24:51 -0500

    move styles out of addedit

--- src/Components/BudgetItemEditors/AddEditBudgetItem.js
+++ src/Components/BudgetItemEditors/AddEditBudgetItem.js
@@ -2,96 +2,24 @@ import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import FormItem from "./FormItem";
 import { getItem, addItem, updateItem, removeItem } from "../Store";
-import styled from "styled-components";
 import {
   ISODateStringToTicks,
   ticksToISODateString,
   getTodayTicks
 } from "../../Utils/dateUtils";
+import {
+  StyledDropDown,
+  FormContainer,
+  AddButtonContainer,
+  StyledButton
+} from "./AddEditBudgetItem.styles";
+import DropdownArrow from "./DropdownArrow";
 
 const DEFAULT_CURRENCY = "default_currency";
 const DEFAULT_LOCATION = "default_location";
 const DEFAULT_PROJECT = "default_project";
 
-const StyledButton = styled.button`
-  background-color: ${props => props.theme.accentOne};
-  color: ${props => props.theme.textInverse};
-  padding: 0.5rem;
-  border-radius: 0.5rem;
-  margin: auto;
-  justify-content: center;
-  width: 100%;
-  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
-    0 4px 6px -2px rgba(0, 0, 0, 0.05);
-  :hover {
-    background-color: ${props => props.theme.accentTwo};
-    color: ${props => props.theme.textNormal};
-  }
-`;
-
-const AddButtonContainer = styled.div`
-  display: flex;
-  width: 100%;
-  padding-right: 1rem;
-  padding-left: 1rem;
-`;
-
-const FormContainer = styled.form`
-  display: flex;
-  flex-wrap: wrap;
-  max-width: 48rem;
-  padding: 2rem 0;
-  margin: auto;
-  margin-bottom: 1rem;
-
-  @media (max-width: ${props => props.theme.breakpoint}) {
-    margin-top: 0.5rem;
-  }
-`;
-
-const StyledDropDown = styled.select`
-  display: block;
-  appearance: none;
-  width: 100%;
-  background-color: #edf2f7;
-  border: 0.0625rem solid ${props => props.theme.accentOne};
-  border-radius: 0.5rem;
-  padding: 0.75rem;
-  line-height: 1.25;
-  :focus {
-    background-color: ${props => props.theme.textInverse};
-  }
-`;
-
-const SvgContainer = styled.div`
-  pointer-events: none;
-  position: absolute;
-  display: flex;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  align-items: center;
-  padding: 0rem 0.5rem;
-`;
-
-const DropdownArrow = () => (
-  <SvgContainer>
-    <svg
-      style={{ fill: "currentColor", height: "1rem", width: "1rem" }}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-    >
-      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-    </svg>
-  </SvgContainer>
-);
-
-const AddEditBudgetItem = ({
-  id,
-  returnAction,
-  categories,
-  accounts
-}) => {
+const AddEditBudgetItem = ({ id, returnAction, categories, accounts }) => {
   const [form, setValues] = useState({
     dateTicks: getTodayTicks(),
     reportingDateTicks: getTodayTicks(),
@@ -183,12 +111,12 @@ const AddEditBudgetItem = ({
 
       if (isSubscribed) {
         const customReportingDate = item.reportingDateTicks !== item.dateTicks;
-        
+
         // If the optional fields are missing add them to prevent attempting to bind
         // to undefined
         if (!item.details) item.details = "";
         if (!item.project) item.project = "";
-        
+
         setValues({ ...item, customReportingDate });
       }
     };
@@ -284,7 +212,6 @@ const AddEditBudgetItem = ({
 
   return (
     <FormContainer onSubmit={handleSubmit}>
-      {/* TODO: These dates are always UTC, should be local */}
       <FormItem
         name="dateTicks"
         label="Date"
