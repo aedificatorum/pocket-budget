4b03f38e60b5e95031a0a3fc33c8166734bc1fbf
Celine <github@celinechole.com>
2020-02-05 17:36:17 -0500

    finish removing framer motion

--- src/Components/Summary/SummaryTable.js
+++ src/Components/Summary/SummaryTable.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
-import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import MediaQuery from "react-responsive";
 import { removeItem, getItemsForPeriod } from "../Store";
