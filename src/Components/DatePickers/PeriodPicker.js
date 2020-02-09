cc0ce1c85b83d352b32beebfa5f6f19b6a09aa5b
Tim Addison <git@tjaddison.com>
2020-02-09 15:31:10 -0500

    move date logic to dedicated file

--- src/Components/DatePickers/PeriodPicker.js
+++ src/Components/DatePickers/PeriodPicker.js
@@ -1,20 +1,13 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import MonthPicker from "./MonthPicker";
-import { ticksToShortDateWithYear, getToday } from "../../Utils/dateUtils";
+import { ticksToShortDateWithYear } from "../../Utils/dateUtils";
 import { SelectYourViewStyle } from "./PeriodPicker.styles";
+import { DateRanges as DateRangeCreator } from "./DateRanges";
 
-const DateRanges = {
-  CalendarMonth: "calendarmonth",
-  YearToDate: "ytd",
-  LastCalendarMonth: "lastcalendarmonth",
-  LastThreeCalendarMonths: "lastthreecalendarmonths",
-  LastYear: "lastyear",
-};
+const DateRanges = DateRangeCreator();
 
 const today = new Date();
-const todayUtc = getToday();
-const startOfYearTicks = moment.utc([today.getFullYear(), 0, 1]).unix() * 1000;
 
 const PeriodPicker = ({ ticks, setTicks }) => {
   // *** Year-Month Code ***
@@ -41,60 +34,22 @@ const PeriodPicker = ({ ticks, setTicks }) => {
   // END: *** Year-Month Code ***
 
   // *** Range picker Code ***
-  const [rangeType, setRangeType] = useState(DateRanges.CalendarMonth);
+  const [rangeType, setRangeType] = useState(DateRanges.CalendarMonth.key);
 
   const handleRangeChange = e => {
     const oldValue = rangeType;
     const newValue = e.target.value;
 
     switch (newValue) {
-      case DateRanges.CalendarMonth:
+      case DateRanges.CalendarMonth.key:
         // update the values of ticks to match the currently selected month
-        if (oldValue !== DateRanges.CalendarMonth) {
+        if (oldValue !== DateRanges.CalendarMonth.key) {
           setTicksToMonth(yearMonth.year, yearMonth.month);
         }
         break;
-      case DateRanges.YearToDate:
-        setTicks({
-          fromTicks: startOfYearTicks,
-          // We add one day to take us to UTC midnight 'tomorrow'
-          toTicks: todayUtc.add(1, "day").unix() * 1000,
-        });
-        break;
-      case DateRanges.LastCalendarMonth:
-        setTicks({
-          fromTicks:
-            getToday()
-              .date(1)
-              .add(-1, "month")
-              .unix() * 1000,
-          toTicks:
-            getToday()
-              .date(1)
-              .unix() * 1000,
-        });
-        break;
-      case DateRanges.LastThreeCalendarMonths:
-        setTicks({
-          fromTicks:
-            getToday()
-              .date(1)
-              .add(-3, "month")
-              .unix() * 1000,
-          toTicks:
-            getToday()
-              .date(1)
-              .unix() * 1000,
-        });
-        break;
-      case DateRanges.LastYear:
-        setTicks({
-          fromTicks: moment.utc([today.getFullYear() - 1, 0, 1]).unix() * 1000,
-          toTicks: startOfYearTicks,
-        });
-        break;
       default:
-        break;
+        const range = DateRanges[newValue];
+        setTicks({fromTicks: range.fromTicks, toTicks: range.toTicks});
     }
 
     setRangeType(newValue);
@@ -124,7 +79,7 @@ const PeriodPicker = ({ ticks, setTicks }) => {
           justifyContent: "space-around",
         }}
       >
-        {rangeType === DateRanges.CalendarMonth && (
+        {rangeType === DateRanges.CalendarMonth.key && (
           <MonthPicker yearMonth={yearMonth} updateMonth={updateMonth} />
         )}
         <div>
@@ -133,10 +88,9 @@ const PeriodPicker = ({ ticks, setTicks }) => {
               <option value={"select"} disabled>
                 Select your view
               </option>
-              <option value={DateRanges.CalendarMonth}>Month</option>
-              <option value={DateRanges.YearToDate}>Year to Date</option>
-              <option value={DateRanges.LastThreeCalendarMonths}>Last 3 Months</option>
-              <option value={DateRanges.LastYear}>Last Year</option>
+              {Object.values(DateRanges).map(dr => {
+                return <option value={dr.key}>{dr.shortName}</option>
+              })}
             </select>
           </SelectYourViewStyle>
         </div>
