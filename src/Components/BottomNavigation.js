39a2bd5b8a57ac60788dcbe7b822305dbef855d2
Tim Addison <git@tjaddison.com>
2020-05-25 20:57:17 -0400

    Update mobile nav

--- src/Components/BottomNavigation.js
+++ src/Components/BottomNavigation.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { signOut } from "./Auth";
 
 const BottomNavigationContainer = styled.div`
   display: flex;
@@ -24,12 +25,105 @@ const StyledLink = styled(Link)`
   font-weight: 600;
 `;
 
+const ButtonStyle = styled.div`
+  display: flex;
+  align-items: center;
+
+  border: 0.0625rem solid white;
+  border-radius: 0.25rem;
+  padding: 0.25rem;
+`;
+
+const SvgStyle = styled.svg`
+  height: 1.5rem;
+  width: 1.5rem;
+  fill: currentColor;
+`;
+
+const FloatingMenu = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row-reverse;
+
+  & > div:first-child {
+    background-color: #ffffff;
+    height: 100%;
+    width: 70%;
+    box-shadow: 0px 0 15px rgba(0, 0, 0, 1);
+    padding: 1.5rem;
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: flex-end;
+  }
+  & > div:nth-child(2) {
+    background-color: #ffffff;
+    opacity: 0.8;
+    width: 30%;
+  }
+`;
+
+const TextStyle = styled.div`
+  text-align: right;
+  display: flex;
+  flex-direction: column;
+  padding-top: 1rem;
+
+  a {
+    display: inline-block;
+    font-weight: 400;
+    font-size: 1.125rem;
+    font-family: "arial";
+    padding: 0.5rem;
+  }
+`;
+
 export const BottomNavigation = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <BottomNavigationContainer>
       <StyledLink to="/">Home</StyledLink>
       <StyledLink to="/quickadd">Quick Add</StyledLink>
       <StyledLink to="/overview">Overview</StyledLink>
+      <ButtonStyle onClick={() => setIsExpanded(true)}>
+        <SvgStyle viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </SvgStyle>
+      </ButtonStyle>
+      {isExpanded && (
+        <FloatingMenu>
+            <TextStyle>
+            <Link onClick={() => setIsExpanded(false)} to="/">
+              Home
+            </Link>
+            <Link onClick={() => setIsExpanded(false)} to="/quickadd">
+              Quick Add
+            </Link>
+            <Link onClick={() => setIsExpanded(false)} to="/overview">
+              Overview
+            </Link>
+            <Link onClick={() => setIsExpanded(false)} to="/fullform">
+              Add
+            </Link>
+            <Link onClick={() => setIsExpanded(false)} to="/summary">
+              Summary
+            </Link>
+            <Link onClick={() => setIsExpanded(false)} to="/admin">
+              Admin
+            </Link>
+            <a href="/" onClick={signOut}>
+              Log Out
+            </a>
+          </TextStyle>
+
+          <div onClick={() => setIsExpanded(false)}></div>
+        </FloatingMenu>
+      )}
     </BottomNavigationContainer>
   );
 };
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,26 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Nav from "./Nav";
-import MobileNav from "./MobileNav";
 
 const HeaderContainer = styled.div`
-  display: flex;
-  flex-direction: row;
   padding: 0.5rem;
   background-color: ${props => props.theme.accentOne};
   color: ${props => props.theme.textInverse};
   font-family: "Julius Sans One", sans-serif;
-
-  div:first-child {
-    font-weight: 600;
-    flex-grow: 1;
-    font-size: 1.5rem;
+  font-weight: 600;
+  font-size: 1.25rem;
 
     @media (min-width: ${props => props.theme.breakpoint}) {
-      & {
-        font-size: 2rem;
-      }
-    }
+      font-size: 2rem;
   }
 `;
 
@@ -31,9 +22,6 @@ const Header = () => {
       <HeaderContainer>
         <div>
           <Link to="/">Pocket Budget</Link>
-        </div>
-        <div>
-          <MobileNav />
         </div>
       </HeaderContainer>
       <Nav />
