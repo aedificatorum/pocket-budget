e5abe28289a1e8bd4002721572acf952067dcfb2
Celine <github@celinechole.com>
2020-06-05 12:38:04 -0400

    update reset button

--- src/Components/BudgetItemEditors/AddEditBudgetItem.js
+++ src/Components/BudgetItemEditors/AddEditBudgetItem.js
@@ -13,7 +13,7 @@ import {
 } from "./AddEditBudgetItem.styles";
 import DropdownArrow from "./DropdownArrow";
 import { useSetNavMenuItems } from "../Provider/NavMenuItemsContext";
-import localStorageKeys from "../Admin";
+import { localStorageKeys } from "../Admin";
 
 console.log(localStorageKeys);
 
@@ -185,18 +185,20 @@ const AddEditBudgetItem = ({ id, returnAction, accounts, initialAccountId, initi
     setMenuItems([
       <FilterButton
         onClick={() => {
-          alert("Reset");
+          localStorageKeys.forEach(k => {
+            localStorage.removeItem(k);
+          });
         }}
       >
         <svg
           fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
           viewBox="0 0 24 24"
           stroke="currentColor"
         >
-          <path d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"></path>
+          <path d="M6 18L18 6M6 6l12 12"></path>
         </svg>
       </FilterButton>,
     ]);
