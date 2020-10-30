1f9d39dd8b8fc6f83ac3ef483d9e4de41d08f858
Tim Addison <git@tjaddison.com>
2020-10-30 16:43:53 -0400

    add bundle size analyzer

--- craco.config.js
+++ craco.config.js
@@ -1,21 +1,29 @@
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 module.exports = function({ env }) {
   const appTarget = process.env.REACT_APP_MEMORY ? "inMemory" : "firestore";
+  const isProductionBuild = process.env.NODE_ENV === "production";
+  const analyzerMode = process.env.REACT_APP_INTERACTIVE_ANALYZE ? "server" : "json";
+
+  const plugins = [new webpack.NormalModuleReplacementPlugin(
+    /(.*)APP_TARGET-(\.*)/,
+    function(resource) {
+      resource.request = resource.request.replace(
+        /APP_TARGET-/,
+        `${appTarget}-`
+      );
+    }
+  )]
+
+  if(isProductionBuild) {
+    plugins.push(new BundleAnalyzerPlugin({analyzerMode}))
+  }
 
   return {
     webpack: {
-      plugins: [
-        new webpack.NormalModuleReplacementPlugin(
-          /(.*)APP_TARGET-(\.*)/,
-          function(resource) {
-            resource.request = resource.request.replace(
-              /APP_TARGET-/,
-              `${appTarget}-`
-            );
-          }
-        )
-      ]
+      plugins
     }
   };
 };
