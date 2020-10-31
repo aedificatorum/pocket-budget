50c8a5fea3a22697da61b3474cd5e362fda3344f
Tim Addison <git@tjaddison.com>
2020-10-31 14:14:42 -0400

    remove moment from dateranges

--- src/Components/DatePickers/DateRanges.js
+++ src/Components/DatePickers/DateRanges.js
@@ -1,9 +1,4 @@
-import moment from "moment";
-import { getToday } from "../../Utils/dateUtils";
-
-const today = new Date();
-const todayUtc = getToday();
-const startOfYearTicks = moment.utc([today.getFullYear(), 0, 1]).unix() * 1000;
+import { getTomorrowTicks, getStartOfMonthTicks, getStartOfYearTicks } from "../../Utils/dateUtils";
 
 const calendarMonth = () => {
   return {
@@ -16,9 +11,9 @@ const yearToDate = () => {
   return {
     key: "YearToDate",
     shortName: "Year to Date",
-    fromTicks: startOfYearTicks,
+    fromTicks: getStartOfYearTicks(),
     // We add one day to take us to UTC midnight 'tomorrow'
-    toTicks: todayUtc.add(1, "day").unix() * 1000,
+    toTicks: getTomorrowTicks(),
   };
 };
 
@@ -26,15 +21,8 @@ const lastThreeCalendarMonths = () => {
   return {
     key: "LastThreeCalendarMonths",
     shortName: "Last 3 Months",
-    fromTicks:
-      getToday()
-        .date(1)
-        .add(-3, "month")
-        .unix() * 1000,
-    toTicks:
-      getToday()
-        .date(1)
-        .unix() * 1000,
+    fromTicks: getStartOfMonthTicks(-3),
+    toTicks:getStartOfMonthTicks(),
   };
 };
 
@@ -42,8 +30,8 @@ const lastYear = () => {
   return {
     key: "LastYear",
     shortName: "Last Year",
-    fromTicks: moment.utc([today.getFullYear() - 1, 0, 1]).unix() * 1000,
-    toTicks: startOfYearTicks,
+    fromTicks: getStartOfYearTicks(-1),
+    toTicks: getStartOfYearTicks(),
   };
 };
 
--- src/Utils/dateUtils.js
+++ src/Utils/dateUtils.js
@@ -6,26 +6,52 @@ export const getToday = () => {
   return moment.utc([localToday.year(), localToday.month(), localToday.date(), 0, 0, 0, 0]);
 };
 
+export const getTomorrowTicks = () => {
+  return getToday().add(1, "day").unix() * 1000;
+};
+
+export const getStartOfMonth = (monthOffset = 0) => {
+  const localToday = moment();
+
+  return moment
+    .utc([localToday.year(), localToday.month(), 1, 0, 0, 0, 0])
+    .add(monthOffset, "month");
+};
+
+export const getStartOfMonthTicks = (monthOffset = 0) => {
+  return getStartOfMonth(monthOffset).unix() * 1000;
+};
+
+export const getStartOfYear = (yearOffset = 0) => {
+  const localToday = moment();
+
+  return moment.utc([localToday.year(), 0, 1, 0, 0, 0, 0]).add(yearOffset, "year");
+};
+
+export const getStartOfYearTicks = (yearOffset = 0) => {
+  return getStartOfYear(yearOffset).unix() * 1000;
+};
+
 export const getTodayTicks = () => {
   return getToday().unix() * 1000;
 };
 
-export const ticksToISODateString = ticks => {
+export const ticksToISODateString = (ticks) => {
   return moment.utc(ticks).format("YYYY-MM-DD");
 };
 
-export const ISODateStringToTicks = isoString => {
+export const ISODateStringToTicks = (isoString) => {
   return moment.utc(isoString).unix() * 1000;
 };
 
-export const ticksToFullDate = ticks => {
+export const ticksToFullDate = (ticks) => {
   return moment.utc(ticks).format("MMMM Do YYYY");
 };
 
-export const ticksToShortDate = ticks => {
+export const ticksToShortDate = (ticks) => {
   return moment.utc(ticks).format("DD MMM");
 };
 
-export const ticksToShortDateWithYear = ticks => {
+export const ticksToShortDateWithYear = (ticks) => {
   return moment.utc(ticks).format("DD MMM YYYY");
 };
