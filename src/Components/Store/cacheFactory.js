aac0cad6800f6a103bff5eafbb081ec3a8f5db55
Tim Addison <git@tjaddison.com>
2021-01-19 19:47:56 -0500

    reuse logic

--- src/Components/Store/cacheFactory.js
+++ src/Components/Store/cacheFactory.js
@@ -20,11 +20,6 @@ export const addCacheToFunctionWithArgs = (
   return (...args) => {
     const cacheKey = cacheKeyBuilderFunc(...args);
 
-    if (!resultCache[cacheKey] || resultCacheDates[cacheKey] < new Date().getTime()) {
-      resultCache[cacheKey] = func(...args);
-      resultCacheDates[cacheKey] = new Date().getTime() + cacheDurationSeconds * 1000;
-    }
-
-    return resultCache[cacheKey];
+    return addCacheToFunction(func, cacheKey, cacheDurationSeconds)(...args)
   };
 };
