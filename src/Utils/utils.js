092fa301e933ddc99f34b560694bb8bd44f02cec
Tim Addison <git@tjaddison.com>
2021-02-14 17:38:00 -0500

    sortBy test passes for func

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,7 +1,15 @@
 export const sortBy = (array, compareFunc) => {
-  if(!compareFunc) {
+  if (!compareFunc) {
     return array.sort();
   }
-  // TODO
+
+  if (typeof compareFunc === "function") {
+    return array.sort((a, b) => {
+      const aComp = compareFunc(a);
+      const bComp = compareFunc(b);
+
+      return aComp > bComp ? 1 : aComp < bComp ? -1 : 0;
+    });
+  }
   return array.sort();
-}
+};
