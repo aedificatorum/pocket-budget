3170ec4f96d191e939cc678a4180ad46aecebd2b
Tim Addison <git@tjaddison.com>
2021-02-16 19:29:30 -0500

    remove lodash from quickadd util

--- src/Components/QuickAdd/utils.js
+++ src/Components/QuickAdd/utils.js
@@ -1,5 +1,17 @@
-import _ from "lodash";
+import { sortBy } from "Utils/utils";
 
 export const getTopToFromItems = (items, topN) => {
-  return _.chain(items).uniqBy("to").sortBy("to").take(topN).value();
+  const sortedItems = sortBy(items, "to");
+
+  // Add one entry for every unique to
+  const uniqueTos = [];
+  let lastTo = undefined;
+  for (let item of sortedItems) {
+    if (lastTo !== item.to) {
+      uniqueTos.push(item);
+      lastTo = item.to;
+    }
+  }
+
+  return uniqueTos.slice(0, topN);
 };
